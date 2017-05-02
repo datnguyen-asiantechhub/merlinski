ed054de54f964d5cbc38a6a9f77a146e5b3d63ef
datnguyen <datnguyen@asiantechhub.com>
2017-05-02 18:44:32 +0700

    update code

---
+++ application/views/pages/resetpass.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+<!--<link rel="stylesheet" href="css/mystyle.css">-->
+<link rel="stylesheet" href="Sass/style.css">
+<script type="text/javascript" src="js/jquery-3.2.1.js"></script>
+	<title></title>
+</head>
+<body>
+<header>
+	<div class="top-bar">
+		<div class="container">
+			<div class="row">
+				<div class="col-sx-12 col-sm-6 col-md-6">
+					<h4>Welcome to FallsCreek</h4>
+				</div>
+				<div class="col-sx-12 col-sm-6 col-md-6">
+					<p><span>0800 80 40 20</span> - 6 Days a week from 9:00am to 22:00pm <a class="btn btn-warning btn-xs" >USD</a></p>
+
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="navigation-bar">
+		<div class="container">
+			<div class="row">
+				<nav class="navbar navbar-default">
+					<a class="navbar-brand" href="">
+						<img src="" alt="FallsCreek">
+					</a>
+					<ul class="nav nav-tab">
+						<li class="active"><a href="">2017 Season Passes</a></li>
+						<li ><a href="">Snow Passes</a></li>
+						<li ><a href="">Snowsport</a></li>
+					</ul>
+					<form class="navbar-form navbar-left" role="search">
+						 <div class="form-group">
+						    <input type="text" class="form-control" placeholder="Search product, catergories...">
+						 </div>
+						 <button type="submit" class="btn btn-default">Submit</button>
+					</form>
+				</nav>
+			</div>
+		<div>
+	</div>
+</header>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+</body>
+</html>
