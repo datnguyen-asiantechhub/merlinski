147cad72c10f5e11fd45a4bc60886bfb0f47e612
datnguyen <datnguyen@asiantechhub.com>
2017-05-03 19:09:12 +0700

    update code

--- application/views/pages/resetpass.php
+++ application/views/pages/resetpass.php
@@ -2,9 +2,9 @@
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-<!--<link rel="stylesheet" href="css/mystyle.css">-->
-<script type="text/javascript" src="js/jquery-3.2.1.js"></script>
+<link rel="stylesheet" type="text/css" href="../../libraries/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="../assets/css/mystyle.css">
+<script type="text/javascript" src="../../libraries/bootstrap/js/jquery.min.js"></script>
 	<title></title>
 </head>
 <body>
@@ -12,20 +12,20 @@
 	<div class="top-bar">
 		<div class="container">
 			<div class="row">
-				<div class="col-sx-12 col-sm-6 col-md-6">
-					<h4>Welcome to FallsCreek</h4>
-					<p><span>0800 80 40 20</span> - 6 Days a week from 9:00am to 22:00pm <a class="btn btn-warning btn-xs" >USD</a></p>
+				<div class="col-sx-12 col-sm-12 col-md-12">
+					<p class="telephone"><span>0800 80 40 20</span> - 6 Days a week from 9:00am to 22:00pm <a class="btn btn-warning btn-xs" >USD</a></p>
+					<h4 class="welcome">Welcome to FallsCreek</h4>
 				</div>	
 			</div>
 		</div>
 	</div>
 	<div class="navigation-bar-1">
 		<div class="container">
-			<nav class="navbar navbar-default">
+			<nav class="navbar">
 				<a class="navbar-brand" href="">
-					<img src="images/logo.jpg" alt="FallsCreek">
+					<img class="img-responsive" src="../assets/images/logo.jpg" alt="FallsCreek">
 				</a>
-				<ul class="nav nav-tab">
+				<ul class="nav navbar-nav head-nav">
 					<li class="dropdown"><a href="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true">2017 Season Passes <span class="caret"></span></a>
 						<ul class="dropdown-menu">
 							<li><a href="">menu</a></li>
@@ -48,11 +48,11 @@
 						</ul>
 					</li>
 				</ul>
-				<form class="navbar-form navbar-left" role="search">
+				<form method="POST" action="" class="navbar-form navbar-left search-form" role="search">
 					 <div class="form-group">
 					    <input type="text" class="form-control" placeholder="Search product, catergories...">
 					 </div>
-					 <button type="submit" class="btn btn-default">Submit</button>
+					 <input type="submit" class="btn btn-default" value="Search" >
 				</form>
 				<div class="">
 					<div class="">
@@ -97,25 +97,104 @@
 		<div class="row">
 			<div class="col-sx-12 col-sm-3 col-md-3">
 				<ul class="nav nav-pills nav-stacked side-nav">
-					<li role="presentation">Account info</li>
-					<li role="presentation">My Tickets <span class="badge">33</span></li>
+					<li data-toggle="popover" role="presentation">Account info
+						<ul class="popover-content" class="list-group">
+							<li class="list-group-item><a href="">info1</a></li>
+							<li class="list-group-item><a href="">info1</a></li>
+							<li class="list-group-item><a href="">info1</a></li>
+						</ul>
+					</li>
+					<li role="presentation">My Tickets <span class="badge">33</span>
+
+					</li>
 					<li role="presentation">My Vouchers</li>
 					<li role="presentation">Order History</li>
 					<li role="presentation">Rental Settings</li>
 					<li role="presentation">Upcoming Event</li>
 				</ul>
 			</div>
-			<div class="col-sx-12 col-sm-9 col-md-3">
+			<div class="col-sx-12 col-sm- col-md-9">
 				<div class="main-content">
-
+					<h1>Change Password</h1>
+					<form method="POST" action="">
+						<input class="form-control" type="Password" name="old_pass" placeholder="Current Password">
+						<input class="form-control" type="Password" name="new_pass" placeholder="Mew Passsword">
+						<input class="form-control" type="Password" name="renew_pass" placeholder="confirm Password">
+						<input class="btn btn-warning btn-lg" type="submit" value="Change">
+					</form>
 				</div>
 			</div>
 		</div>
 	</div>
 </main>
 <footer>
-
+	<div class="container">
+		<div class="row">
+			<ul class="footer-menu">
+				<li><h1>Contact Us</h1>
+					<ul>
+						<li><a href=""><span></span>EMail us</a></li>
+						<li><span></span> 1234 3456</li>
+						<li><span></span> whatsApp us on 432143211233</li>
+						<li><a href=""><span></span>Find Resort near you</a></li>
+						<li><a href=""><span></span>Need Helps?</a></li>
+						<li><a href=""><span></span>Sign up for our newsletter!</a></li>
+					<ul>
+				</li>
+				<li><h1>Discover</h1>
+					<ul>
+						<li><a href="">Online payment</a></li>
+						<li><a href="">Delivery</a></li>
+						<li><a href="">Returns</a></li>
+						<li><a href="">General terms of booking</a></li>
+						<li><a href="">Order tracking</a></li>
+						<li><a href="">Secrurity and privacy</a></li>
+					<ul>
+				</li>
+				<li><h1>Company</h1>
+					<ul>
+						<li><a href="">About us</a></li>
+						<li><a href="">Our resorts</a></li>
+						<li><a href="">Our environmental commitments</a></li>
+						<li><a href="">Investor:see our key financial figures</a></li>
+						<li><a href="">Professional: take a look at the pro area</a></li>
+						<li><a href="">Jounalists: enter press area</a></li>
+						<li><a href="">Job seeker: browse our offerings</a></li>
+					<ul>
+				</li>
+				<li><h1>let us search for you</h1>
+					<ul>
+						<li>
+							<form method="POST" action="">
+								<input type="text" name="foot_search">
+								<input type="submit" name="search">
+							</form>
+						</li>
+						<li>
+							<a href=""><span></span></a>
+							<a href=""><span></span></a>
+							<a href=""><span></span></a>
+							<a href=""><span></span></a>
+							<a href=""><span></span></a>
+							<a href=""><span></span></a>
+							<a href=""><span></span></a>
+						</li>
+					<ul>
+				</li>
+			</ul>
+		</div>
+	</div>
 </footer>
-<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="../../libraries/bootstrap/js/bootstrap.min.js"></script>
+<script type="text/javascript">
+	$(document).ready(function(){
+		$('[data-toggle="popover]').popover({
+			html:true,
+			content:function(){
+				retrun $('ul.popover-content').html();
+			}
+		});
+	});
+</script>
 </body>
 </html>
