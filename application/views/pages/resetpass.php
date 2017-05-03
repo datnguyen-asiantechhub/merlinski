5bd529d6004ba872488a1f17ba13fb3d795e263b
datnguyen <datnguyen@asiantechhub.com>
2017-05-03 15:10:48 +0700

    update trang Login

--- application/views/pages/resetpass.php
+++ application/views/pages/resetpass.php
@@ -4,7 +4,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 <!--<link rel="stylesheet" href="css/mystyle.css">-->
-<link rel="stylesheet" href="Sass/style.css">
 <script type="text/javascript" src="js/jquery-3.2.1.js"></script>
 	<title></title>
 </head>
@@ -15,37 +14,108 @@
 			<div class="row">
 				<div class="col-sx-12 col-sm-6 col-md-6">
 					<h4>Welcome to FallsCreek</h4>
-				</div>
-				<div class="col-sx-12 col-sm-6 col-md-6">
 					<p><span>0800 80 40 20</span> - 6 Days a week from 9:00am to 22:00pm <a class="btn btn-warning btn-xs" >USD</a></p>
+				</div>	
+			</div>
+		</div>
+	</div>
+	<div class="navigation-bar-1">
+		<div class="container">
+			<nav class="navbar navbar-default">
+				<a class="navbar-brand" href="">
+					<img src="images/logo.jpg" alt="FallsCreek">
+				</a>
+				<ul class="nav nav-tab">
+					<li class="dropdown"><a href="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true">2017 Season Passes <span class="caret"></span></a>
+						<ul class="dropdown-menu">
+							<li><a href="">menu</a></li>
+							<li><a href="">menu</a></li>
+							<li><a href="">menu</a></li>
+						</ul>
+					</li>
+					<li class="dropdown"><a href="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true">Snow Passes <span class="caret"></span></a>
+						<ul class="dropdown-menu">
+							<li><a href="">menu</a></li>
+							<li><a href="">menu</a></li>
+							<li><a href="">menu</a></li>
+						</ul>
+					</li>
+					<li class="dropdown"><a href="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true">Snowsport <span class="caret"></span></a>
+						<ul class="dropdown-menu">
+							<li><a href="">menu</a></li>
+							<li><a href="">menu</a></li>
+							<li><a href="">menu</a></li>
+						</ul>
+					</li>
+				</ul>
+				<form class="navbar-form navbar-left" role="search">
+					 <div class="form-group">
+					    <input type="text" class="form-control" placeholder="Search product, catergories...">
+					 </div>
+					 <button type="submit" class="btn btn-default">Submit</button>
+				</form>
+				<div class="">
+					<div class="">
+						<span class=""></span>
+					</div>
+					<dvi class="">
+						<span class=""></span>
+					</dvi>
+				</div>
+			</nav>
+		</div>
+	</div>
+	<div class="title">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-sm-6 col-md-6">
+					<ol class="breadcrumb">
+						<li><a href="">HOME</a></li>
+						<li><a href="">MY ACCOUNT</a></li>
+						<li><a class="active">CHANGE PASSWORD</a></li>
+					</ol>
+				</div>
+				<div class="col-xs-12 col-sm-6 col-md-6">
+					<label>View</label>
+					<select class="selectpicker select-view">
+						<option>24</option>
+						<option>16</option>
+						<option>10</option>
+					</select>
+					<label>Sort by Price </label>
+					<select class="selectpicker select-sort">
+						<option>High/Low</option>
+						<option>Low/High</option>
+					</select>
+				</div>
+			</div>
+		</div>
+	</div>
+</header>
+<main>
+	<div class="container">
+		<div class="row">
+			<div class="col-sx-12 col-sm-3 col-md-3">
+				<ul class="nav nav-pills nav-stacked side-nav">
+					<li role="presentation">Account info</li>
+					<li role="presentation">My Tickets <span class="badge">33</span></li>
+					<li role="presentation">My Vouchers</li>
+					<li role="presentation">Order History</li>
+					<li role="presentation">Rental Settings</li>
+					<li role="presentation">Upcoming Event</li>
+				</ul>
+			</div>
+			<div class="col-sx-12 col-sm-9 col-md-3">
+				<div class="main-content">
 
 				</div>
 			</div>
 		</div>
 	</div>
-	<div class="navigation-bar">
-		<div class="container">
-			<div class="row">
-				<nav class="navbar navbar-default">
-					<a class="navbar-brand" href="">
-						<img src="" alt="FallsCreek">
-					</a>
-					<ul class="nav nav-tab">
-						<li class="active"><a href="">2017 Season Passes</a></li>
-						<li ><a href="">Snow Passes</a></li>
-						<li ><a href="">Snowsport</a></li>
-					</ul>
-					<form class="navbar-form navbar-left" role="search">
-						 <div class="form-group">
-						    <input type="text" class="form-control" placeholder="Search product, catergories...">
-						 </div>
-						 <button type="submit" class="btn btn-default">Submit</button>
-					</form>
-				</nav>
-			</div>
-		<div>
-	</div>
-</header>
+</main>
+<footer>
+
+</footer>
 <script type="text/javascript" src="js/bootstrap.min.js"></script>
 </body>
 </html>
