44412b460421f7fe9d912efa4490744c26156aa8
datnguyen <datnguyen@asiantechhub.com>
2017-05-04 16:51:58 +0700

    update code

--- application/views/pages/admin/resetpass.php
+++ application/views/pages/admin/resetpass.php
@@ -2,9 +2,9 @@
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" type="text/css" href="../../libraries/bootstrap/css/bootstrap.min.css">
-<link rel="stylesheet" href="../assets/css/mystyle.css">
-<script type="text/javascript" src="../../libraries/bootstrap/js/jquery.min.js"></script>
+<link rel="stylesheet" type="text/css" href="../../../libraries/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="../../assets/css/mystyle.css">
+<script type="text/javascript" src="../../../libraries/bootstrap/js/jquery.min.js"></script>
 	<title></title>
 </head>
 <body>
@@ -22,8 +22,8 @@
 	<div class="navigation-bar-1">
 		<div class="container">
 			<nav class="navbar">
-				<a class="navbar-brand" href="">
-					<img class="img-responsive" src="../assets/images/logo.jpg" alt="FallsCreek">
+				<a class="logo" href="">
+					<img class="img-responsive" src="../../assets/images/logo.jpg" alt="FallsCreek">
 				</a>
 				<ul class="nav navbar-nav head-nav">
 					<li class="dropdown"><a href="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true">2017 Season Passes <span class="caret"></span></a>
@@ -49,10 +49,10 @@
 					</li>
 				</ul>
 				<form method="POST" action="" class="navbar-form navbar-left search-form" role="search">
-					 <div class="form-group">
-					    <input type="text" class="form-control" placeholder="Search product, catergories...">
+					 <div class="form-group search-box">
+					    <input type="text" class="form-control search-text" placeholder="Search product, catergories...">
+					    <input type="submit" class="btn search-button" >
 					 </div>
-					 <input type="submit" class="btn btn-default" value="Search" >
 				</form>
 				<div class="">
 					<div class="">
@@ -76,17 +76,25 @@
 					</ol>
 				</div>
 				<div class="col-xs-12 col-sm-6 col-md-6">
-					<label>View</label>
-					<select class="selectpicker select-view">
-						<option>24</option>
-						<option>16</option>
-						<option>10</option>
-					</select>
-					<label>Sort by Price </label>
-					<select class="selectpicker select-sort">
-						<option>High/Low</option>
-						<option>Low/High</option>
-					</select>
+					<div class="view-sort">
+						<ul>
+							<li>
+								<label>View</label>
+								<select class="selectpicker select-view">
+									<option>24</option>
+									<option>16</option>
+									<option>10</option>
+								</select>
+							</li>
+							<li>
+								<label>Sort by Price </label>
+								<select class="selectpicker select-sort">
+									<option>High/Low</option>
+									<option>Low/High</option>
+								</select>
+							</li>
+						</ul>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -96,28 +104,24 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-sx-12 col-sm-3 col-md-3">
-				<ul class="nav nav-pills nav-stacked side-nav">
-					<li data-toggle="popover" role="presentation">Account info
-						<ul class="popover-content" class="list-group">
-							<li class="list-group-item><a href="">info1</a></li>
-							<li class="list-group-item><a href="">info1</a></li>
-							<li class="list-group-item><a href="">info1</a></li>
-						</ul>
-					</li>
-					<li>My Tickets <span class="badge">33</span></li>
-					<li>My Vouchers</li>
-					<li>Order History</li>
-					<li>Rental Settings</li>
-					<li>Upcoming Event</li>
-				</ul>
+				<div class="left-side">
+					<ul class="nav nav-pills nav-stacked sidenav">
+						<li><a href="">Account info <span class="glyphicon glyphicon-chevron-right"></a></li>
+						<li><a href="">My Tickets <span class="badge">33</span> <span class="glyphicon glyphicon-chevron-right"></a></li>
+						<li><a href="">My Vouchers</a></li>
+						<li><a href="">Order History</a></li>
+						<li><a href="">Rental Settings</a></li>
+						<li><a href="">Upcoming Event</a></li>
+					</ul>
+				</div>
 			</div>
 			<div class="col-sx-12 col-sm- col-md-9">
 				<div class="main-content">
 					<h1>Change Password</h1>
-					<form method="POST" action="">
+					<form class="change-pass" method="POST" action="">
 						<input class="form-control" type="Password" name="old_pass" placeholder="Current Password">
 						<input class="form-control" type="Password" name="new_pass" placeholder="Mew Passsword">
-						<input class="form-control" type="Password" name="renew_pass" placeholder="confirm Password">
+						<input class="form-control" type="Password" name="renew_pass" placeholder="Confirm Password">
 						<input class="btn btn-warning btn-lg" type="submit" value="Change">
 					</form>
 				</div>
@@ -128,8 +132,9 @@
 <footer>
 	<div class="container">
 		<div class="row">
-			<ul class="footer-menu">
-				<li><h1>Contact Us</h1>
+			<div class="col-sx-12 col-sm-3 col-md-3">
+				<h3>Contact Us</h3>
+				<div class="footer-block">
 					<ul>
 						<li><a href=""><span></span>EMail us</a></li>
 						<li><span></span> 1234 3456</li>
@@ -137,9 +142,12 @@
 						<li><a href=""><span></span>Find Resort near you</a></li>
 						<li><a href=""><span></span>Need Helps?</a></li>
 						<li><a href=""><span></span>Sign up for our newsletter!</a></li>
-					<ul>
-				</li>
-				<li><h1>Discover</h1>
+					</ul>
+				</div>
+			</div>
+			<div class="col-sx-12 col-sm-3 col-md-3">
+				<h3>Discover</h3>
+				<div class="footer-block">
 					<ul>
 						<li><a href="">Online payment</a></li>
 						<li><a href="">Delivery</a></li>
@@ -147,9 +155,12 @@
 						<li><a href="">General terms of booking</a></li>
 						<li><a href="">Order tracking</a></li>
 						<li><a href="">Secrurity and privacy</a></li>
-					<ul>
-				</li>
-				<li><h1>Company</h1>
+					</ul>
+				</div>
+			</div>
+			<div class="col-sx-12 col-sm-3 col-md-3">
+				<h3>Company</h3>
+				<div class="footer-block">
 					<ul>
 						<li><a href="">About us</a></li>
 						<li><a href="">Our resorts</a></li>
@@ -158,31 +169,40 @@
 						<li><a href="">Professional: take a look at the pro area</a></li>
 						<li><a href="">Jounalists: enter press area</a></li>
 						<li><a href="">Job seeker: browse our offerings</a></li>
-					<ul>
-				</li>
-				<li><h1>let us search for you</h1>
+					</ul>
+				</div>
+			</div>
+			<div class="col-sx-12 col-sm-3 col-md-3">
+				<h3>Let us search for you</h3>
+				<div class="footer-block">
 					<ul>
 						<li>
 							<form method="POST" action="">
-								<input type="text" name="foot_search">
-								<input type="submit" name="search">
+								<div class="search-box">
+									<input class="form-control footer-search" type="text" name="foot_search">
+									<input class="btn search-button" type="submit" name="search">
+								</div>
 							</form>
+							<p class="search-tip">Popular item: Ski, resort, outdoor, booking, daytime</p>
 						</li>
-						<li>
-							<a href=""><span></span></a>
-							<a href=""><span></span></a>
-							<a href=""><span></span></a>
-							<a href=""><span></span></a>
-							<a href=""><span></span></a>
-							<a href=""><span></span></a>
-							<a href=""><span></span></a>
+						<li class="link-social">
+							<a  href=""><span class="sc-blog"></span></a>
+							<a  href=""><span class="sc-print"></span></a>
+							<a  href=""><span class="sc-instagram"></span></a>
+							<a  href=""><span class="sc-youtube.com"></span></a>
+							<a  href=""><span class="sc-facebook"></span></a>
+							<a  href=""><span class="sc-tweeter"></span></a>
+							<a  href=""><span class="sc-google"> </span></a>
 						</li>
-					<ul>
-				</li>
-			</ul>
+					</ul>
+				</div>
+			</div>
 		</div>
 	</div>
+	<div class="copyright">
+		<p class="text-center">Copyright 2017 FallsCreek. All right reserved.</p>
+	</div>
 </footer>
-<script type="text/javascript" src="../../libraries/bootstrap/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="../../../libraries/bootstrap/js/bootstrap.min.js"></script>
 </body>
 </html>
