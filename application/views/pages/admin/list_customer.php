f3799adab28693c194b11dde1eff2a804a7bdb43
Trang <trangtran0194@gmail.com>
2017-05-08 07:11:01 +0700

    Trang#

--- application/views/pages/admin/list_customer.php
+++ application/views/pages/admin/list_customer.php
@@ -46,28 +46,17 @@
             </tbody>
         </table>
     </div>
-    <nav aria-label="Page navigation">
-        <ul class="pagination">
-
-            <li><a href="#">1</a></li>
-            <li><a href="#">2</a></li>
-            <li><a href="#">3</a></li>
-            <li><a href="#">4</a></li>
-            <li><a href="#">5</a></li>
-
-        </ul>
-        <div class="clearfix"></div>
-        <ul class="pagination">
-            <li>
-                <a href="#" aria-label="Previous">
-                    <span aria-hidden="true">&laquo;</span>
-                </a>
-            </li>
-            <li>
-                <a href="#" aria-label="Next">
-                    <span aria-hidden="true">&raquo;</span>
-                </a>
-            </li>
-        </ul>
-    </nav>
+    <select id="pagination-select">
+        <option>1</option>
+        <option>2</option>
+        <option>3</option>
+        <option>4</option>
+        <option>5</option>
+        <option>6</option>
+        <option>7</option>
+    </select>
+    <ul class="pager">
+        <li><a href="#"><i class="fa  fa-angle-left"></i></a></li>
+        <li><a href="#"><i class="fa  fa-angle-right"></i></a></li>
+    </ul>
 </div>
