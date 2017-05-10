c78ca0f995397bdbb8a47c73d806feedbd395958
Trang <trangtran0194@gmail.com>
2017-05-10 22:20:08 +0700

    Trang#fixadmin_leftmenu

--- application/views/templates/admin_leftmenu.php
+++ application/views/templates/admin_leftmenu.php
@@ -1,45 +1,44 @@
-
 <div class="left-side">
-	<ul class="nav nav-pills nav-stacked sidenav">
-		<li class="sub-menu">
-			<a data-toggle="collapse" data-target="#accountinfo" href="#">Account info <span class="glyphicon glyphicon-chevron-right right-arrow"></span></a>
-			<ul class="nav nav-stacked collapse" id="accountinfo">
-			    <li><a href="#">Menu1</a></li>
-			    <li><a href="#">Menu2</a></li>
-			    
-			</ul>
-		</li>
-		<li class="sub-menu">
-			<a data-toggle="collapse" data-target="#mytickets" href="#">My Tickets <span class="badge">33</span> <span class="glyphicon glyphicon-chevron-right right-arrow"></span></a>
-			<ul class="nav nav-stacked collapse" id="mytickets">
-			    <li><a href="#">Menu1</a></li>
-			    <li><a href="#">Menu2</a></li>
+    <ul class="nav nav-pills nav-stacked sidenav">
+        <li><a href="">Home</a> </li>
+        <li class="sub-menu">
+            <a data-toggle= "collapse" data-target="#products" href="#">Product<span class="glyphicon glyphicon-chevron-right right-arrow"></span></a>
+            <ul class="nav nav-stacked collapse" id="products">
+                <li><a href="">Select1</a></li>
+                <li><a href="">Select2</a></li>
+            </ul>
+        </li>
+        <li class="sub-menu">
+            <a data-toggle="collapse" data-target="#Order" href="#"> Order <span class="badge">33</span> <span class="glyphicon glyphicon-chevron-right right-arrow"></span></a>
+            <ul class="nav nav-stacked collapse" id="Order">
+                <li><a href="#">Select1</a></li>
+                <li><a href="#">Select2</a></li>
+            </ul>
+        </li>
+        <li><a href="">Customer</a></li>
+        <li class="sub-menu">
+            <a data-toggle="collapse" data-target="#passcard" href="#"> Pass Card<span class="glyphicon glyphicon-chevron-right right-arrow"></span></a>
+            <ul class="nav nav-stacked collapse" id="passcard">
+                <li><a href="">Select1</a></li>
+                <li><a href="">Select2</a></li>
+            </ul>
+        </li>
+        <li class="sub-menu">
+            <a data-toggle="collapse" data-target="#voucher" href="#">Voucher<span class="glyphicon glyphicon-chevron-right right-arrow"></span></a>
+            <ul class="nav nav-stacked collapse" id="voucher">
+                <li><a href="">Select1</a></li>
+                <li><a href="">Select2</a></li>
+            </ul>
 
+        </li>
+        <li class="sub-menu">
+            <a data-toggle="collapse" data-target="#report" href="#">Report<span class=" glyphicon glyphicon-chevron-right right-arrow"></span></a>
+            <ul class="nav nav-stacked collapse" id="report">
+                <li><a href="">Select1</a></li>
+                <li><a href="">Select2</a></li>
 
-<div class="menu-left">
-	<ul class="nav nav-pills nav-stacked sidenavv">
-		<li><a href="">Home</a></li>
-		<li class="sub-menu">
-			<a data-toggle="collapse" data-target="#products" href="#"> Product <span class="glyphicon glyphicon-chevron-right right-arrowa"></span></a>
-			<ul class="nav nav-stacked collapse" id="products">
-			    <li><a href="#">Select1</a></li>
-			    <li><a href="#">Select2</a></li>
-			    
-			</ul>
-		</li>
-		<li class="sub-menu">
-			<a data-toggle="collapse" data-target="#Order" href="#"> Order <span class="badgee">33</span> <span class="glyphicon glyphicon-chevron-right right-arrowa"></span></a>
-			<ul class="nav nav-stacked collapse" id="Order">
-			    <li><a href="#">Select1</a></li>
-			    <li><a href="#">Select2</a></li>
-			</ul>
-		</li>
-		<li><a href="">Customer</a></li>
-		<li><a href="">Pass Card</a></li>
-		<li><a href="">Voucher</a></li>
-		<li><a href="">Report</a></li>
-		<li><a href="">Configuration</a></li>
-	</ul>
-</div>
-
+            </ul>
+        </li>
+        <li><a href="">Configuration</a></li>
+    </ul>
 </div>
